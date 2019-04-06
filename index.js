05e531f6166d73e3534581561b9aee6f1999852a
Damir Bajramovic <bajramovic.damir@hotmail.com>
2019-04-06 13:36:11 +0200

    Add kitty examples and animals.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// Variables:
+// var a; // NEMOJTE KORISTIT VAR AKO NE ZNATE ZASTO TO RADITE
+// let a; // nekonstantna varijabla, mozete dodijeliti
+// const a; // Konstantna vrijednost, ne mozete dodijeliti konstantnoj varijabli
+
+// Primjer za require i module.exports
+// console.log(require("./kitty"));
+
+
+// PRIMJERI SA VAR: 
+
+// // 1
+// console.log(a);
+
+// var a = "nesto";
+
+// console.log(a);
+
+// DOWNLOAD GIT FOR WINDOWS!
+// https://git-scm.com/download/win
+// run "git init" in cmd in the project directory 
+// .gitignore fajl za ignorisanje stvari
+// OBAVEZNO ignorisati fajlove kao sto je node_modules
+// Ne zelite da ubacujete fajlove na git koji su nepotrebni
---
+++ kitty.js
@@ -0,0 +1,3 @@
+console.log("FAJL POCINJE DA SE IZVRSAVA!");
+
+module.exports = "HELLO Kitty!"
